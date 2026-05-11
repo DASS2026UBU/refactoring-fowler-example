a08c1659346d7157ac22eed1e0e01a5adb3ddc40
dandaluz4 <daa1005@alu.ubu.es>
2026-05-11 13:05:08 +0200

    Move method 'amountFor' (Issue #1)

--- src/ubu/gii/dass/refactoring/Customer.java
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -59,22 +59,10 @@ public class Customer {
 		return result;
 	}
 
+	/**
+	 * @deprecated Use {@link ubu.gii.dass.refactoring.Rental#amountFor(double)} instead
+	 */
 	private double amountFor(double thisAmount, Rental each) {
-		switch (each.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			thisAmount += 2;
-			if (each.getDaysRented() > 2)
-				thisAmount += (each.getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			thisAmount += each.getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (each.getDaysRented() > 3)
-				thisAmount += (each.getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return thisAmount;
+		return each.amountFor(thisAmount);
 	}
 }
