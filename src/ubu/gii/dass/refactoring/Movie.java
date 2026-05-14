9a1fa98a2006deb0a25df8af7080c9d3952d174f
rrf1003 <rrf1003@alu.ubu.es>
2026-05-14 17:40:22 +0200

    Refactor: Replace Type Code with State/Strategy in Movie (Issue #2)

--- src/ubu/gii/dass/refactoring/Movie.java
+++ src/ubu/gii/dass/refactoring/Movie.java
@@ -17,19 +17,31 @@ public class Movie {
 	public static final int NEW_RELEASE = 1;
 
 	private String _title;
-	private int _priceCode;
+	private MoviePrice _price;
 
 	public Movie(String title, int priceCode) {
 		_title = title;
-		_priceCode = priceCode;
+		setPriceCode(priceCode);
 	}
 
 	public int getPriceCode() {
-		return _priceCode;
+		return _price.getPriceCode();
 	}
 
 	public void setPriceCode(int arg) {
-		_priceCode = arg;
+		switch (arg) {
+		case REGULAR:
+			_price = new RegularPrice();
+			break;
+		case CHILDRENS:
+			_price = new ChildrenPrice();
+			break;
+		case NEW_RELEASE:
+			_price = new NewReleasePrice();
+			break;
+		default:
+			throw new IllegalArgumentException("Incorrect Price Code");
+		}
 	}
 
 	public String getTitle() {
@@ -40,7 +52,7 @@ public class Movie {
 	 * @deprecated Use {@link #getCharge(int)} instead
 	 */
 	double getCharge(Rental rental) {
-		return getCharge(null);
+		return getCharge(0);
 	}
 
 	double getCharge(int daysRented) {
@@ -67,7 +79,7 @@ public class Movie {
 	 * @deprecated Use {@link #getFrequentRenterPoints(int)} instead
 	 */
 	int getFrequentRenterPoints(Rental rental) {
-		return getFrequentRenterPoints(null);
+		return getFrequentRenterPoints(0);
 	}
 
 	int getFrequentRenterPoints(int daysRented) {
